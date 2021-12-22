70157c53fad406c56b38a2f839ff4a9d868e722c
moos <herget06@gmail.com>
2021-12-22 09:54:19 -0500

    css for main page done still need media querie adjustments

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ function drinkLoop (response) {
     strDrinkThumb.style.width = '200px'
     strDrinkThumb.style.height = 'auto'
     drinkDiv.setAttribute('id', drinkData[i].idDrink)
-    drinkName.setAttribute('style', 'background: #20201d; color: #fff; padding: 5px; opacity: 0.85;')
+    drinkName.setAttribute('style', 'width: 200px; background: #20201d; color: #fff; padding: 5px; opacity: 0.85;')
     drinkName.innerText = drinkData[i].strDrink
     drinkList.appendChild(drinkDiv)
     drinkDiv.appendChild(drinkName)
